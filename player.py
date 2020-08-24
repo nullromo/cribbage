7d3dbf4fe87a918154df1911346c5c5f8c8f3ce6
Kyle Kovacs <kylerobertkovacs@gmail.com>
2020-08-23 21:33:40 -0700

    finished fully-interactable human player

--- player.py
+++ player.py
@@ -1,6 +1,16 @@
 """
 Representations for Players
 """
+def check_playable(card, played_cards):
+    """
+    Given a list of played cards, tells whether the given
+    card is playable or not.
+    """
+    count = sum(map(lambda card: card.value, played_cards))
+    if count + card.value > 31:
+        return False
+    return True
+
 class Player:
     """
     Represents a player in the game. The base player just makes
@@ -14,12 +24,11 @@ class Player:
     def __str__(self):
         return self.name + ' ' + str(self.hand_cards) + ' (' + str(self.points) + ')'
 
-    def throw_to_crib(self, dealer):
+    def throw_to_crib(self, _):
         """
         Takes 2 cards out of self.hand_cards and returns them
         as a list.
         """
-        print(self.hand_cards, dealer)
         return [self.hand_cards.pop(), self.hand_cards.pop()]
 
     def play_card(self, played_cards):
@@ -27,11 +36,8 @@ class Player:
         Removes and returns a card from self.hand_cards if there
         is a playable card. Otherwise, returns None (go).
         """
-        #TODO: should depend on all information
-        print(self.hand_cards)
-        count = sum(map(lambda card: card.value, played_cards))
         for card in self.hand_cards:
-            if count + card.value > 31:
+            if not check_playable(card, played_cards):
                 continue
             self.hand_cards.remove(card)
             return card
@@ -41,31 +47,53 @@ class HumanPlayer(Player):
     """
     Player that takes decisions from stdin.
     """
+    def select_card(self):
+        """
+        Allows a card to be selected from the player's hand.
+        """
+        print('Enter a number to select the corresponding card.')
+        print(self.hand_cards)
+        for arrow in [True, False]:
+            print(' ', end='')
+            for (i, card) in enumerate(self.hand_cards):
+                if card.rank == 10:
+                    print(' ', end='')
+                print(('^' if arrow else str(i + 1)) + '   ', end='')
+            print()
+        index = -1
+        while index < 0:
+            try:
+                index = int(input())
+                if index < 1 or index > len(self.hand_cards):
+                    index = -1
+                    raise ValueError()
+            except ValueError:
+                print('Please enter a number between 1 and ' + str(len(self.hand_cards)))
+        selected_card = self.hand_cards[index - 1]
+        self.hand_cards.remove(selected_card)
+        return selected_card
+
     def throw_to_crib(self, dealer):
         cards_to_throw = []
+        print('Throw 2 cards into your' + (' ' if dealer else " opponent's ") + 'crib.')
         for card_name in ['first', 'second']:
             print()
             print('Throw the ' + card_name + ' card.')
-            print('Enter a number to throw the corresponding card.')
-            print(self.hand_cards)
-            for arrow in [True, False]:
-                print(' ', end='')
-                for (i, card) in enumerate(self.hand_cards):
-                    if card.rank == 10:
-                        print(' ', end='')
-                    print(('^' if arrow else str(i + 1)) + '   ', end='')
-                print()
-            index_to_throw = -1
-            while index_to_throw < 0:
-                try:
-                    index_to_throw = int(input())
-                    if index_to_throw < 1 or index_to_throw > len(self.hand_cards):
-                        index_to_throw = -1
-                        raise ValueError()
-                except ValueError:
-                    print('Please enter a number between 1 and ' + str(len(self.hand_cards)))
-            card_to_throw = self.hand_cards[index_to_throw - 1]
+            card_to_throw = self.select_card()
             cards_to_throw.append(card_to_throw)
-            self.hand_cards.remove(card_to_throw)
         print()
         return cards_to_throw
+
+    def play_card(self, played_cards):
+        for card in self.hand_cards:
+            if check_playable(card, played_cards):
+                print()
+                print('Play a card.')
+                selected_card = self.select_card()
+                if check_playable(selected_card, played_cards):
+                    return selected_card
+                print(str(selected_card) + ' is not playable. Select a different card.')
+                self.hand_cards.append(selected_card)
+                return self.play_card(played_cards)
+        print('No playable cards left.')
+        return None
